73250a4f4700ea1e4682f642c0a18864be176954
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-10-06 00:07:18 +0530

    Add files via upload

---
+++ Programming_with_JavaScript/Exercise_Declaring_variables.js
@@ -0,0 +1,12 @@
+var petDog = "Rex";     // Task 1 solution
+console.log(petDog);
+var petCat = "Pepper";  // Task 2 solution
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+var catSound = "purr";  // Task 7 solution
+var dogSound = "woof";  // Task 8 solution
+console.log(petDog,"says",dogSound);
+console.log(petCat,"says",catSound);
+var catSound = "meow";  // Task 11 solution
+console.log(petCat,"now says",catSound);
---
+++ Programming_with_JavaScript/var.js
@@ -0,0 +1,15 @@
+/*
+"Jhon"
+'Jhon'
+"Hello, Jhon"
+'Hello, Jhon'
+*/
+// variable declaration
+var person ;
+var person = "Jhon" ;
+console.log("Hello",person);
+var greeting = "Hello!!" ;
+console.log(greeting,person) ;
+greeting = "Hi" ;
+person = "Rita" ;
+console.log(greeting,person) ;
