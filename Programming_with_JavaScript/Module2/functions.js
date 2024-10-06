5fe46de276c5e8638d5e3b086a889a77d7d68d83
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-10-06 14:25:12 +0530

    Update functions.js

--- Programming_with_JavaScript/Module2/functions.js
+++ Programming_with_JavaScript/Module2/functions.js
@@ -31,3 +31,11 @@ function listArrayItems(arr) {
 }
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors);
+
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length ; i++ ) {
+        console.log(i+1, arr[i]); //display the array item where the index is euqal to i
+    }
+}
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
