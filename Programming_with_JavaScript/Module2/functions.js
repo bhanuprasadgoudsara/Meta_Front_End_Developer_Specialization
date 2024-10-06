4a25545468083ad01359546a0594de5dcb8c58ff
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-10-06 14:26:34 +0530

    Update functions.js

--- Programming_with_JavaScript/Module2/functions.js
+++ Programming_with_JavaScript/Module2/functions.js
@@ -39,3 +39,12 @@ function listArrayItems(arr) {
 }
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors);
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == 'red') {
+            console.log(i*100, "tomato!")
+        } else {
+            console.log(i*100, arr[i])
+        }
+    }
+}
