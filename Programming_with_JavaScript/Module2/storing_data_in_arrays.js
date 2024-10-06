521c663a5c5114a221ec7ca934dca119cf7aed37
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-10-06 11:42:14 +0530

    Create storing_data_in_arrays.js

---
+++ Programming_with_JavaScript/Module2/storing_data_in_arrays.js
@@ -0,0 +1,13 @@
+var carriage0 = "wheat";
+console.log(carriage0);
+var carriage0 = "wheat";
+var carriage1 = "barley";
+var carriage2 = "salt";
+var carriage3 = "potato";
+var carriage4 = "rocks";
+var train2carriage0 = 1;
+var train2carriage1 = 2;
+//array:
+var train1 = [];
+var train1 = ["wheat", "barley", "salt", "potato", "rocks"];
+console.log(train1[0]); //wheat
