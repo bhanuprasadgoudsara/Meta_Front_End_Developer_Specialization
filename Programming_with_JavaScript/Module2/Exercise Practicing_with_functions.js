d61b4a685147ff0e972667ba45e26ff7801bb140
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-10-06 14:32:52 +0530

    Create Exercise Practicing_with_functions.js

---
+++ Programming_with_JavaScript/Module2/Exercise Practicing_with_functions.js
@@ -0,0 +1,14 @@
+//Task1:
+function letterFinder(word, match) {
+  //Code
+}
+//Task2:
+function letterFinder(word, match) {
+  for (var i = 0; i < word.length; i++) {
+    //
+  }
+}
+//Task3:
+//Task4:
+//Task5:
+//Task6:
